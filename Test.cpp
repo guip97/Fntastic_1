3aed3d2f102856f1ee5436cd5cd66917886b8900
guip97 <66515017+guip97@users.noreply.github.com>
2022-02-28 17:32:07 +1000

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <sstream>
+
+int main()
+{
+	std::string InputString;
+	std::getline(std::cin, InputString);
+
+	int InputLength = InputString.length();
+
+	for (int i = 0; i < InputLength; i++)
+	{
+		bool bUnique = true;
+		char Symbol = InputString[i];
+
+		for (int j = 0; j < InputLength; j++)
+		{
+			if (i != j && std::tolower(Symbol) == std::tolower(InputString[j]))
+			{
+				InputString[j] = ')';
+				bUnique = false;
+			}
+		}
+
+		InputString[i] = bUnique ? '(' : ')';
+	}
+
+	std::cout << InputString << "\n";
+}
